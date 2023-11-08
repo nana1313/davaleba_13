883e52341aac70b9dfea29b54b841c7c53a25ad5
nana dumbadze <55438500+nana1313@users.noreply.github.com>
2023-11-09 01:26:21 +0400

    add js script

---
+++ script.js
@@ -0,0 +1,36 @@
+// davaleba 1
+var width = 10;
+var height = 15.6;
+var name = "nana";
+var lastname = "dumbadze";
+var istrue = true;
+var isfalse = false;
+const person = {firstname: "nana", lastname: "dumbadze"};
+const brands = ["zara", "nike", "adidas", "vans"];
+
+//davaleba 2
+var x = 30;
+var y = 15;
+var a;
+a=x+y;
+var b;
+b=x-y;
+var c;
+c=x*y;
+var d;
+d=x/y;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+//davaleba 3
+console.log("what is your name :" + " " + name);
+console.log(`what is your lastname : ${lastname}`);
+
+//davaleba4
+var sigrdze = 7;
+var sigane = 5;
+var area;
+area = sigrdze * sigane;
+console.log(`martkutxedis fartobi = ${area}`);
